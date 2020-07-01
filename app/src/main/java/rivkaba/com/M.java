ab3f126fa269cbe7fbb73695361f8d1630c67ffa
Rivka <rbir359718@gmail.com>
2020-07-01 17:39:34 +0300

    try

---
+++ app/src/main/java/rivkaba/com/M.java
@@ -0,0 +1,14 @@
+package rivkaba.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class M extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m);
+    }
+}
