3951a030fe0e321243abbf9be154445e5240278d
Rivka <rbir359718@gmail.com>
2020-08-26 16:11:02 +0300

    view

--- app/src/main/java/rivkaba/com/Video_View.java
+++ app/src/main/java/rivkaba/com/Video_View.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.VideoView;
 
-public class UploadVideo extends AppCompatActivity {
-
+public class Video_View extends AppCompatActivity {
+    private VideoView videoView;
+    private ImageButton ply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload_video);
-
+        setContentView(R.layout.activity_video__view);
+        videoView=(VideoView)findViewById(R.id.videoView2);
+        ply=(ImageButton)findViewById(R.id.ply);
     }
 }
