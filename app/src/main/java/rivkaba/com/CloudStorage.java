82f7e5df3ec2add374332c8356fb0deab0638461
Rivka <rbir359718@gmail.com>
2020-08-26 15:47:11 +0300

    video

--- app/src/main/java/rivkaba/com/CloudStorage.java
+++ app/src/main/java/rivkaba/com/CloudStorage.java
@@ -5,30 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+//import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class CloudStorage extends AppCompatActivity {
-    private FirebaseFirestore ff;
-    private static final  int REQUEST_CODE=101;
+    //FirebaseAuth auth;
+  //  private static final  int REQUEST_CODE=101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloud_storage);
-        ff= FirebaseFirestore.getInstance();
-        if(ff.getCurrentUser()!=null)
-        {
-            startActivity(new Intent(this,UploadVideo.class));
-            finish();
-        }else {
-            authenticateUser();
-        }
-        }
-       private void authenticateUser(){
-        List<AuthUI.IdpConfig> providers=new ArrayList<>();
-           providers.add(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+        //auth= FirebaseAuth.getInstance();
+       // if(auth.getCurrentUser()!=null)
+      //  {
+       //     startActivity(new Intent(this,UploadVideo.class));
+       //     finish();
+     //   }else {
+      //      authenticateUser();
+      //  }
+     //   }
+    //   private void authenticateUser(){
+       // List<AuthUI.IdpConfig> providers=new ArrayList<>();
+       //    providers.add(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+       //    startActivityForResult(AuthUI.getInstance().createSignInIntenBuilder().setAvilableProviders(providers)
+       //    .setIsSmartLockEnabled(false).build().REQEST_CODE);
+   // }
+    //protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+     //   super.onActivityResult(requestCode, resultCode, data);
+       // IdpResponse response=IdpResponse.f
     }
     }
 
