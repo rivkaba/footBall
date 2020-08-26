269fd06aeae2c899d5dd7b39c53ec569eb84dd1d
Rivka <rbir359718@gmail.com>
2020-08-26 17:25:56 +0300

    d video

--- app/src/main/java/rivkaba/com/Video_View.java
+++ app/src/main/java/rivkaba/com/Video_View.java
@@ -17,7 +17,7 @@ public class Video_View extends AppCompatActivity {
         setContentView(R.layout.activity_video__view);
         videoView=(VideoView)findViewById(R.id.videoView2);
         ply=(ImageButton)findViewById(R.id.ply);
-        videoUri= Uri.parse("gs://football-b0d61.appspot.com/MVI_1085.MOV");
+        videoUri= Uri.parse("https://firebasestorage.googleapis.com/v0/b/football-b0d61.appspot.com/o/MVI_1085.MOV?alt=media&token=49923e04-c9e1-4c55-87b9-1dc5b8a3d651");
         videoView.setVideoURI(videoUri);
         videoView.requestFocus();
         videoView.start();
