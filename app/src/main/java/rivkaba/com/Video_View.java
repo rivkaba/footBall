62f79cdd3b8285050bdcc55d4e278d86c3ff5878
Rivka <rbir359718@gmail.com>
2020-08-27 18:22:26 +0300

    duration

--- app/src/main/java/rivkaba/com/Video_View.java
+++ app/src/main/java/rivkaba/com/Video_View.java
@@ -3,26 +3,41 @@ package rivkaba.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 public class Video_View extends AppCompatActivity {
+    private TextView durationTimer;
     private VideoView videoView;
     private ImageButton play;
     private Uri videoUri;
     private boolean isPlaying= false;
+    private  int current=0;
+    private int duration=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video__view);
+        durationTimer=(TextView) findViewById(R.id.duration);
         videoView=(VideoView)findViewById(R.id.videoView2);
         play=(ImageButton)findViewById(R.id.ply);
         videoUri= Uri.parse("https://firebasestorage.googleapis.com/v0/b/football-b0d61.appspot.com/o/video.mp4?alt=media&token=0b639468-c1b7-4622-b247-1f4816c8396e");
         videoView.setVideoURI(videoUri);
         videoView.requestFocus();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                duration=mediaPlayer.getDuration()/1000;
+                String durationString= String.format("%02d:%02d",duration/60,duration%60);
+                durationTimer.setText(durationString);
+            }
+
+            });
         videoView.start();
         isPlaying=true;
         play.setImageResource(R.mipmap.pause_action);
