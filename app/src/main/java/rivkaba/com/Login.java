957b48cd209299f0c703333a6d262936fcfe68fd
Rivka <rbir359718@gmail.com>
2021-01-16 21:25:24 +0200

    16/1

--- app/src/main/java/rivkaba/com/Login.java
+++ app/src/main/java/rivkaba/com/Login.java
@@ -3,6 +3,7 @@ package rivkaba.com;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +13,14 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
@@ -25,7 +33,7 @@ public class Login extends AppCompatActivity {
     private Button forgetP;
     private Context context;
     private Button singUp;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +49,7 @@ public class Login extends AppCompatActivity {
 
 
         forgetP=(Button) findViewById(R.id.forgrtp);
-
+        mAuth = FirebaseAuth.getInstance();
 
         switchU.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -82,7 +90,7 @@ public class Login extends AppCompatActivity {
 
 
 
-
+                    signIndDatabase(editEmaill.getText().toString(), edtPassWord.getText().toString());
 
                        // String myName = edtName.getText().toString();
                        // SharedPreferences.Editor editorName = getSharedPreferences("name", MODE_PRIVATE).edit();
@@ -98,6 +106,7 @@ public class Login extends AppCompatActivity {
         }
 
         });
+
 
                 forgetP.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -115,7 +124,45 @@ public class Login extends AppCompatActivity {
 
         });
                     }
+    private void signIndDatabase(String email, String password) {
+        // Log.d(TAG, "signIn:" + email);
+        // if (!validateForm()) {
+        //    return;
+        // }
 
+        // showProgressBar();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            // Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //  updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("enter", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // updateUI(null);
+                            // [START_EXCLUDE]
+                            //  checkForMultiFactorFailure(task.getException());
+                            // [END_EXCLUDE]
+                        }
+
+                        // [START_EXCLUDE]
+                         /*   if (!task.isSuccessful()) {
+                                mBinding.status.setText(R.string.auth_failed);
+                            }*/
+                        //   hideProgressBar();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
 
             }
 
